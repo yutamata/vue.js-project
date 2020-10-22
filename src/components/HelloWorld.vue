42a85b6ed413e9db1d837200f8956b06b72254b8
Yuta <yutawatanabe@YWnoMacBook-Pro.local>
2020-10-22 14:33:24 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,7 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <button>ログイン</button>
   </div>
 </template>
 
@@ -53,6 +54,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #85b942;
 }
 </style>
